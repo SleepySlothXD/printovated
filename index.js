86a83b88172e243ba95707c68ddeaf12ac2ca969
SleepySlothXD <augiejuricic@gmail.com>
2026-02-24 14:43:10 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+// Start Popout Menu
+const hammenu = document.getElementById('ham-menu');
+const popout = document.getElementById('popout');
+
+hammenu.addEventListener('click', () => {
+  popout.classList.toggle('active');
+  hammenu.classList.toggle('active');
+});
+addEventListener('click', (event) => {
+  if (!hammenu.contains(event.target) && !popout.contains(event.target)) {
+    popout.classList.remove('active');
+    hammenu.classList.remove('active');
+  }
+});
+// End Popout Menu
